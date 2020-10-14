3435ca53f1c5faafd27fb8147845a5aea40e8c2d
willmotil <willmotil@users.noreply.github.com>
2020-10-14 05:36:47 -0400

    Prep for a direct hrdi test
    ... prep to test the following  load by format, get faces into a rendertarget cube alternately set to rendertarget2d then set to cube faces, test cube formats - i didn't give that much thought till now i could probably get around it but that would raise questions about penaltys. Test hammerslay on mips for cubes in shader this is related to the first test. Ect.

--- src/WillDxSharpGltf/Program.cs
+++ src/WillDxSharpGltf/Program.cs
@@ -8,7 +8,8 @@ namespace WillDxSharpGltf
         static void Main()
         {
             //using (var game = new Game1()) game.Run();
-            using (var game = new Game2()) game.Run();
+            //using (var game = new Game2()) game.Run();
+            using (var game = new Game_HdrGpuSideTest()) game.Run();
         }
     }
 }
